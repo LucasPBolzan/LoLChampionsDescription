316897bf4ef4117907a1a6166a0b1af52a963dfe
xeizygg <samuelbs99@hotmail.com>
2024-10-17 14:49:08 -0300

    Uptades FiveXFiveActivity

--- app/src/main/java/com/project/lolchampions/FiveXFiveActivity.kt
+++ app/src/main/java/com/project/lolchampions/FiveXFiveActivity.kt
@@ -1,6 +1,5 @@
 package com.project.lolchampions
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -34,16 +33,13 @@ import kotlinx.coroutines.launch
 @Composable
 fun FiveXFiveActivity() {
     val scope = rememberCoroutineScope()
-    val characterList = remember { mutableStateOf<List<Character>>(emptyList()) }
     val teamOne = remember { mutableStateOf<List<Character>>(emptyList()) }
     val teamTwo = remember { mutableStateOf<List<Character>>(emptyList()) }
     val context = LocalContext.current
 
-
     fun showNotification(context: Context) {
         val channelId = "5x5_generated_channel"
-        val notificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
@@ -56,19 +52,9 @@ fun FiveXFiveActivity() {
             notificationManager.createNotificationChannel(channel)
         }
 
-        val notificationId = 101
-        val builder = NotificationCompat.Builder(context, channelId)
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setContentTitle("Times 5x5 Gerados")
-            .setContentText("Os times do 5x5 foram gerados com sucesso!")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    context,
-                    android.Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED) {
-
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(
                     context as Activity,
                     arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
@@ -78,51 +64,61 @@ fun FiveXFiveActivity() {
             }
         }
 
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("Times 5x5 Gerados")
+            .setContentText("Os times do 5x5 foram gerados com sucesso!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
         with(NotificationManagerCompat.from(context)) {
-            notify(notificationId, builder.build())
+            notify(101, builder.build())
         }
     }
 
     fun generateTeams() {
         scope.launch {
-            val fetchedCharacters = fetchCharacters()
-
-            if (fetchedCharacters.size >= 10) {
-                val shuffledCharacters = fetchedCharacters.shuffled()
-                teamOne.value = shuffledCharacters.take(5)
-                teamTwo.value = shuffledCharacters.takeLast(5)
-
-                showNotification(context)
+            try {
+                val fetchedCharacters = fetchCharacters()
+                if (fetchedCharacters.size >= 10) {
+                    val shuffledCharacters = fetchedCharacters.shuffled()
+                    teamOne.value = shuffledCharacters.take(5)
+                    teamTwo.value = shuffledCharacters.takeLast(5)
+                    showNotification(context)
+                } else {
+                    Toast.makeText(context, "Não há personagens suficientes para gerar os times.", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(context, "Erro ao buscar personagens.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    fun shareTeams() {
-        val teamOneText = teamOne.value.joinToString(separator = "\n") { character ->
-            "${character.name} - ${character.title}"
+    fun saveTeams(teamOne: List<Character>, teamTwo: List<Character>) {
+        try {
+            saveTeamsToFile(context, teamOne, teamTwo)
+            Toast.makeText(context, "Times 5x5 salvos com sucesso!", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(context, "Erro ao salvar os times.", Toast.LENGTH_SHORT).show()
         }
-        val teamTwoText = teamTwo.value.joinToString(separator = "\n") { character ->
-            "${character.name} - ${character.title}"
-        }
+    }
 
-        val shareText = """
-            Time 1:
-            $teamOneText
-            
-            Time 2:
-            $teamTwoText
-        """.trimIndent()
+    fun formatTeamText(teamName: String, team: List<Character>): String {
+        return team.joinToString(separator = "\n") { character -> "${character.name} - ${character.title}" }
+            .let { "$teamName:\n$it" }
+    }
+
+    fun shareTeams(teamOne: List<Character>, teamTwo: List<Character>, context: Context) {
+        val teamOneText = formatTeamText("Time 1", teamOne)
+        val teamTwoText = formatTeamText("Time 2", teamTwo)
+        val shareText = "$teamOneText\n\n$teamTwoText"
 
         val intent = Intent(Intent.ACTION_SEND).apply {
             type = "text/plain"
             putExtra(Intent.EXTRA_TEXT, shareText)
         }
         context.startActivity(Intent.createChooser(intent, "Compartilhar Times 5x5"))
-    }
-
-    fun saveTeams() {
-        saveTeamsToFile(context, teamOne.value, teamTwo.value)
-        Toast.makeText(context, "Times 5x5 salvos com sucesso!", Toast.LENGTH_SHORT).show()
     }
 
     LaunchedEffect(Unit) {
@@ -154,6 +150,7 @@ fun FiveXFiveActivity() {
                 modifier = Modifier.padding(bottom = 8.dp)
             )
         }
+
         items(teamOne.value.zip(positions)) { (character, position) ->
             CharacterListItemWithPosition(character = character, position = position)
         }
@@ -165,13 +162,14 @@ fun FiveXFiveActivity() {
                 modifier = Modifier.padding(vertical = 16.dp)
             )
         }
+
         items(teamTwo.value.zip(positions)) { (character, position) ->
             CharacterListItemWithPosition(character = character, position = position)
         }
 
         item {
             Button(
-                onClick = { shareTeams() },
+                onClick = { shareTeams(teamOne.value, teamTwo.value, context) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 16.dp)
@@ -182,7 +180,7 @@ fun FiveXFiveActivity() {
 
         item {
             Button(
-                onClick = { saveTeams() },
+                onClick = { saveTeams(teamOne.value, teamTwo.value) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 16.dp)

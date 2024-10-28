8c716c0577d6369ed810a29ddd7938aeb3d50500
xeizygg <samuelbs99@hotmail.com>
2024-10-28 19:59:30 -0300

    add full api

--- app/src/main/java/com/project/lolchampions/fetchCharacters.kt
+++ app/src/main/java/com/project/lolchampions/fetchCharacters.kt
@@ -9,50 +9,65 @@ import java.net.URL
 suspend fun fetchCharacters(): List<Character> {
     return withContext(Dispatchers.IO) {
         val characters = mutableListOf<Character>()
-        val url = URL("http://girardon.com.br:3001/champions")
-        val urlConnection = url.openConnection() as HttpURLConnection
-        urlConnection.requestMethod = "GET"
+        var page = 1
+        val size = 20
 
-        return@withContext try {
-            val inputStream = urlConnection.inputStream
-            val result = inputStream.bufferedReader().use { it.readText() }
+        while (true) {
+            val url = URL("http://girardon.com.br:3001/champions?page=$page&size=$size")
+            val urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.requestMethod = "GET"
 
-            val jsonArray = JSONArray(result)
+            try {
+                val inputStream = urlConnection.inputStream
+                val result = inputStream.bufferedReader().use { it.readText() }
 
-            for (i in 0 until jsonArray.length()) {
-                val characterJson = jsonArray.getJSONObject(i)
+                val jsonArray = JSONArray(result)
 
-                val id = characterJson.getString("id")
-                val key = characterJson.getString("key")
-                val name = characterJson.getString("name")
-                val title = characterJson.getString("title")
-                val lore = characterJson.getString("description")
-
-                val icon = if (characterJson.has("icon") && !characterJson.isNull("icon")) {
-                    characterJson.getString("icon")
-                } else {
-                    ""
+                if (jsonArray.length() == 0) {
+                    break // No more data
                 }
 
-                val tags = characterJson.getJSONArray("tags").let { tagsArray ->
-                    List(tagsArray.length()) { index -> tagsArray.getString(index) }
-                }
-                val statsJson = characterJson.getJSONObject("stats")
-                val stats = Stats(
-                    hp = statsJson.getInt("hp"),
-                    mp = statsJson.getInt("mp"),
-                    movespeed = statsJson.getInt("movespeed"),
-                    armor = statsJson.getInt("armor"),
-                    spellblock = statsJson.getDouble("spellblock"),
-                    attackdamage = statsJson.getInt("attackdamage")
-                )
+                for (i in 0 until jsonArray.length()) {
+                    val characterJson = jsonArray.getJSONObject(i)
 
-                characters.add(Character(id, key, name, title, lore, tags, stats, icon))
+                    val id = characterJson.getString("id")
+                    val key = characterJson.getString("key")
+                    val name = characterJson.getString("name")
+                    val title = characterJson.getString("title")
+                    val lore = characterJson.getString("description")
+
+                    val icon = if (characterJson.has("icon") && !characterJson.isNull("icon")) {
+                        characterJson.getString("icon")
+                    } else {
+                        ""
+                    }
+
+                    val tags = characterJson.getJSONArray("tags").let { tagsArray ->
+                        List(tagsArray.length()) { index -> tagsArray.getString(index) }
+                    }
+                    val statsJson = characterJson.getJSONObject("stats")
+                    val stats = Stats(
+                        hp = statsJson.getInt("hp"),
+                        mp = statsJson.getInt("mp"),
+                        movespeed = statsJson.getInt("movespeed"),
+                        armor = statsJson.getInt("armor"),
+                        spellblock = statsJson.getDouble("spellblock"),
+                        attackdamage = statsJson.getInt("attackdamage")
+                    )
+
+                    characters.add(Character(id, key, name, title, lore, tags, stats, icon))
+                }
+
+                if (jsonArray.length() < size) {
+                    break // Reached the last page
+                }
+
+                page += 1 // Move to the next page
+            } finally {
+                urlConnection.disconnect()
             }
-
-            characters
-        } finally {
-            urlConnection.disconnect()
         }
+
+        return@withContext characters
     }
 }
